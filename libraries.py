de400795836f9b18092664a87f4d8d662c6759f3
Gavin Ni <gavinni@Gavins-MacBook-Pro-2.local>
2022-07-26 13:40:44 -0400

    submit

--- libraries.py
+++ libraries.py
@@ -1,54 +1,9 @@
-from cmath import sqrt
-import math
-import random
-
-x = math.pow(5,4)
-print("result 1 is", x)
-
-y = math.sqrt(16)
-print("y is", y)
-
-a = random.randint(0,100)
-print("a is", a)
-
-names = ["Max", "Matthew", "Attila", "Tobal", "Brayson", "Evan"]
-print("Original names: ", names)
-
-random.shuffle(names)
-print("Names after shuffling: ",names)
-
-chosen = random.choice(names)
-print("Chosen name is: ",chosen)
-
-
-from math import pow 
-from math import sqrt
-
-import random
-
-x = math.pow(5,4)
-print("result 1 is", x)
-
-y = math.sqrt(16)
-print("y is", y)
-
-a = random.randint(0,100)
-print("a is", a)
-
-names = ["Max", "Matthew", "Attila", "Tobal", "Brayson", "Evan"]
-print("Original names: ", names)
-
-random.shuffle(names)
-print("Names after shuffling: ",names)
-
-chosen = random.choice(names)
-print("Chosen name is: ",chosen)
 
 import math
 import random
 
 x = math.pow(5,4)
-print("result 1 is", x)
+print("x is", x)
 
 y = math.sqrt(16)
 print("y is", y)
